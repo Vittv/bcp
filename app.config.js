71b823172c1ec0f030b0ca5bf7b89a8a0e18f4b6
Vittv <166668150+Vittv@users.noreply.github.com>
2026-09-08 14:58:32 -0300

    feat(icons): adopt cross icon set across app and android chrome

--- app.config.js
+++ app.config.js
@@ -24,7 +24,7 @@ module.exports = {
     },
     android: {
       adaptiveIcon: {
-        backgroundColor: "#E6F4FE",
+        backgroundColor: "#7A3040",
         foregroundImage: "./assets/android-icon-foreground.png",
         backgroundImage: "./assets/android-icon-background.png",
         monochromeImage: "./assets/android-icon-monochrome.png",
